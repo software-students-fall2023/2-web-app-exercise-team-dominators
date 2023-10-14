b2cd86a870108a7f7565baa7a0e94f25d40e98c1
Ethan Sha <ys4323@nyu.edu>
2023-10-14 01:32:50 -0400

    Add database to connection and test

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+# try using ATLAS for shared clusters,
+# @Database name = ticket_monster, USer as tmpt collection name, first run works fine.
+ATLAS_URI = "mongodb+srv://ys4323:Syysyysyy1!!@cluster0.ocmpb3f.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(ATLAS_URI)
+db = client.ticket_monster
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = db.USer.find()
+    return render_template('index.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
